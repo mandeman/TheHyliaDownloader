64bdd75d214bc80604f42158d4acd4e768650e2c
mandeman <abhijitmande1024@gmail.com>
2016-09-01 04:42:39 +0530

    Add files via upload

--- TheHyliaCrawler.py
+++ TheHyliaCrawler.py
@@ -1,16 +1,17 @@
 from bs4 import BeautifulSoup
 import urllib.request
 import requests
+from config import IP_ADDRESS
 
 
-def linkget(url, loc):
+def get_link(url, loc):
     sc = requests.get(url).text
     soup = BeautifulSoup(sc, 'html.parser')
     i = 0
 
-    for links in soup.findAll('a'):
-        if r'http://anime.thehylia.com/soundtracks/' in links.get('href'):
-            downloader(links.get('href'), loc)
+    for link in soup.findAll('a'):
+        if r'http://anime.thehylia.com/soundtracks/' in link.get('href'):
+            downloader(link.get('href'), loc)
             i += 1
             # print(links.get('href'))
     if i == 0:
@@ -22,21 +23,21 @@ def linkget(url, loc):
 def downloader(songpage, loc):
     sc = requests.get(songpage).text
     soup = BeautifulSoup(sc, 'html.parser')
-    for links in soup.findAll('a'):
-        if r'http://anime.thehylia.com/soundtracks/' in links.get('href'):
-            print("Downloading " + links.string)
-            wapas(links.get('href'), loc, links.string)
+    for link in soup.findAll('a'):
+        if r'http://anime.thehylia.com/soundtracks/' in link.get('href'):
+            print("Downloading " + link.string)
+            wapas(link.get('href'), loc, link.string)
 
 
 def wapas(downloadpage, loc, songname):
     sc = requests.get(downloadpage).text
     soup = BeautifulSoup(sc, 'html.parser')
-    for links in soup.findAll('a'):
-        if (r'http://50.7.54.34/' in links.get('href')):
-            urllib.request.urlretrieve(links.get('href'), loc + '\\' + songname + '.mp3')
+    for link in soup.findAll('a'):
+        if (IP_ADDRESS in link.get('href')):
+            urllib.request.urlretrieve(link.get('href'), loc + '\\' + songname + '.mp3')
             print(songname + " done.")
 
 
 query = input(r"What anime's soundtrack are you looking for?")
 loc = input("Enter download location : ")
-linkget(r"http://anime.thehylia.com/search?search=" + query, loc)
+get_link(r"http://anime.thehylia.com/search?search=" + query, loc)
